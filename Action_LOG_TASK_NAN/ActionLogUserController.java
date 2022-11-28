91a71b98b8b4c1c315bf02049930fb4705d59988
raghu <raghuciet@gmail.com>
2022-11-28 19:16:36 +0530

    by nantha on 28/11/2022

---
+++ Action_LOG_TASK_NAN/ActionLogUserController.java
@@ -0,0 +1,509 @@
+/**
+ * 
+ */
+package mkcl.oesserver.controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mkcl.oes.commons.SessionHelper;
+import mkcl.oespcs.model.Client;
+import mkcl.oesserver.services.OESClientUserServicesImpl;
+import mkcl.oesserver.services.UserActivityLoggerServicesImpl;
+import mkcl.oesserver.services.UserSessionServicesImpl;
+import mkcl.os.apps.model.Constants;
+import mkcl.os.apps.model.Pagination;
+import mkcl.os.apps.utilities.MKCLUtility;
+import mkcl.os.apps.utilities.PaginationHelper;
+
+/**
+ * @author R@ghu
+ * ## BR 25028
+ * 
+ */
+@Controller
+@RequestMapping("actionLog")
+public class ActionLogUserController extends PaginationHelper implements
+		ApplicationContextAware {
+
+	//Log
+	private static final Logger LOGGER = LoggerFactory.getLogger(ActionLogUserController.class);
+	
+	//List Service Implementation Class
+	private OESClientUserServicesImpl oesClientUserServicesImplObj;
+	private UserSessionServicesImpl userServicesImpl;
+	private UserActivityLoggerServicesImpl userActivityServicesImpl;
+	
+	private static final String PAGINATION = "pagination";
+	
+	//List Binding
+	private static final String USERLIST = "UserList";
+	private static final String USERSESSION = "UserSessionList";
+	private static final String USERSUMMARYSESSION = "UserSessionSummaryList";
+	
+    //Redirect Page
+	private static final String USERLISTVIEW = "actionLog/userList";
+	private static final String USERSESSIONLISTVIEW = "actionLog/userSessionLogList";
+	private static final String USERSUMMARYSESSIONLISTVIEW = "actionLog/userActivityLogList";
+	
+	
+	/*
+	 * Get errorPage and exception string constants from constants class.
+	 */
+	private static final String ERRORPAGE = Constants.ERRORPAGE;
+	private static final String EXCEPTIONSTRING = Constants.EXCEPTIONSTRING;
+
+	
+
+	
+	/**
+	 * Method to bind date with with given format.
+	 * 
+	 * @param dataBinder
+	 * @param locale
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder, Locale locale,
+			HttpServletRequest request) {
+		Properties properties = null;
+		String dateFormatString;
+		SimpleDateFormat dateFormat;
+
+		properties = MKCLUtility.loadMKCLPropertiesFile();
+
+		dateFormatString = properties.getProperty("global.dateFormat");
+
+		dateFormat = new SimpleDateFormat(dateFormatString);
+		dateFormat.setLenient(false);
+		dataBinder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, true));
+	}
+
+	/**
+	 * Get method for User List
+	 * @param model
+	 * @param request
+	 * @param locale
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/userLogList" }, method = RequestMethod.GET)
+	public String listGet(Model model, HttpServletRequest request, Locale locale) {
+		LoggerFactory.getLogger(ActionLogUserController.class).debug(
+				"Method Started");
+		try {
+
+			if (request.getSession().getAttribute("loginStatus") == null) {
+				return "redirect:../login/loginpage";
+			}
+			// 1 Call to method add message.
+			addMessage(request, model, locale);
+			
+			// 2 Get searchText.
+			String searchText = request.getParameter("searchText");
+
+			// get OES client id from session
+			Client client = SessionHelper.getClient(request);
+			model.addAttribute("clientID", client.getClientID());
+
+			// 3 Get Object list.
+			Pagination pagination = new Pagination();
+			pagination.setPropertyName("");
+			pagination.setListName(USERLIST);
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("");
+				oesClientUserServicesImplObj.pagination(null, pagination,
+						searchText, model);
+			} else {
+				oesClientUserServicesImplObj.pagination(null, pagination,
+						null, model);
+			}
+			return USERLISTVIEW;
+		} catch (Exception ex) {
+
+			LoggerFactory.getLogger(ActionLogUserController.class).error(
+					ex.getMessage());
+			model.addAttribute(Constants.EXCEPTIONSTRING, ex.getMessage());
+			return Constants.ERRORPAGE;
+		}
+	}
+
+	
+	@RequestMapping(value = { "/userSessionLogList" }, method = RequestMethod.GET)
+	public String listGetSession(Model model, HttpServletRequest request, Locale locale) {
+		
+		LoggerFactory.getLogger(ActionLogUserController.class).debug(
+				"Method Started");
+		try {
+
+			
+			  if (request.getSession().getAttribute("loginStatus") == null) { return
+			  "redirect:../login/loginpage"; }
+			 
+			// 1 Call to method add message.
+			addMessage(request, model, locale);
+			
+			// 2 Get searchText.
+			String searchText = request.getParameter("searchText");
+
+			// get user id and user name from Request
+			model.addAttribute("userId", request.getParameter("userId"));
+			model.addAttribute("userName", request.getParameter("userName"));
+			
+			// get user Id from Request
+			//model.addAttribute("userId", "recruitlive");
+
+			// 3 Get Object list.
+			Pagination pagination = new Pagination();
+			pagination.setPropertyName("");
+			pagination.setListName(USERSESSION);
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("");
+				userServicesImpl.pagination(null, pagination,
+						searchText, model);
+			} else {
+				userServicesImpl.pagination(null, pagination,
+						null, model);
+			}
+			return USERSESSIONLISTVIEW;
+		} catch (Exception ex) {
+
+			LoggerFactory.getLogger(ActionLogUserController.class).error(
+					ex.getMessage());
+			model.addAttribute(Constants.EXCEPTIONSTRING, ex.getMessage());
+			return Constants.ERRORPAGE;
+		}
+	}
+	
+	
+	@RequestMapping(value = { "/userActivityLogList" }, method = RequestMethod.GET)
+	public String listGetSUmmary(Model model, HttpServletRequest request, Locale locale) {
+		
+		LoggerFactory.getLogger(ActionLogSummaryController.class).debug(
+				"Method Started");
+		try {
+
+			
+			  if (request.getSession().getAttribute("loginStatus") == null) { return
+			  "redirect:../login/loginpage"; }
+			 
+			// 1 Call to method add message.
+			addMessage(request, model, locale);
+			
+			// 2 Get searchText.
+			String searchText = request.getParameter("searchText");
+
+			// get user session id,start time,end time,user id,user name, from Request
+			model.addAttribute("userSessionId", request.getParameter("userSessionId"));
+			model.addAttribute("startTime", request.getParameter("startTime"));
+			model.addAttribute("endTime", request.getParameter("endTime"));
+			model.addAttribute("userId", request.getParameter("userId"));
+			model.addAttribute("userName", request.getParameter("userName"));
+			
+			// get user Id from Request
+			//model.addAttribute("userSessionId", "8");
+
+			// 3 Get Object list.
+			Pagination pagination = new Pagination();
+			pagination.setPropertyName("");
+			pagination.setListName(USERSUMMARYSESSION);
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("");
+				userActivityServicesImpl.pagination(null, pagination,
+						searchText, model);
+			} else {
+				userActivityServicesImpl.pagination(null, pagination,
+						null, model);
+			}
+			return USERSUMMARYSESSIONLISTVIEW;
+		} catch (Exception ex) {
+
+			LoggerFactory.getLogger(ActionLogSummaryController.class).error(
+					ex.getMessage());
+			model.addAttribute(Constants.EXCEPTIONSTRING, ex.getMessage());
+			return Constants.ERRORPAGE;
+		}
+	}
+	
+
+
+	
+	/**
+	 * Post method for User List Pagination - Previous
+	 * @param model
+	 * @param pagination
+	 * @param request
+	 * @param searchText
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/prevActionSession" }, method = RequestMethod.POST)
+	public String prevPostActionSession(Model model,
+			@ModelAttribute(PAGINATION) Pagination pagination,
+			HttpServletRequest request, String searchText) {
+
+
+				// get user Id from Request
+				model.addAttribute("userId", request.getParameter("userId"));
+				model.addAttribute("userName", request.getParameter("userName"));
+
+		pagination.setListName(USERSESSION);
+		try {
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("sessionId");
+				userServicesImpl.paginationPrev(null, pagination,
+						searchText, model);
+			} else {
+				userServicesImpl.paginationPrev(null, pagination,
+						searchText, model);
+			}
+		} catch (Exception ex) {
+			LOGGER.error("exception while calling paginationPrev method.");
+		}
+		return USERSESSIONLISTVIEW;
+
+	}
+	
+	/**
+	 * Post method for User List Pagination - Next
+	 * @param model
+	 * @param pagination
+	 * @param request
+	 * @param searchText
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/nextActionSession" }, method = RequestMethod.POST)
+	public String nextPostActionSession(Model model,
+			@ModelAttribute(PAGINATION) Pagination pagination,
+			String searchText, HttpServletRequest request) {
+
+		// get user Id from Request
+		model.addAttribute("userId", request.getParameter("userId"));
+		model.addAttribute("userName", request.getParameter("userName"));
+		
+		
+		pagination.setListName(USERSESSION);
+		try {
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("sessionId");
+				userServicesImpl.paginationNext(null, pagination,
+						searchText, model);
+			} else {
+				userServicesImpl.paginationNext(null, pagination,
+						searchText, model);
+			}
+		} catch (Exception ex) {
+			LOGGER.error("exception while calling nextPost OESCLIENT User Controller  method.");
+		}
+		return USERSESSIONLISTVIEW;
+	}
+
+	
+
+	
+	
+	/**
+	 * Post method for User List Pagination - Previous
+	 * @param model
+	 * @param pagination
+	 * @param request
+	 * @param searchText
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/prevActionSummary" }, method = RequestMethod.POST)
+	public String prevPostActionSummary(Model model,
+			@ModelAttribute(PAGINATION) Pagination pagination,
+			HttpServletRequest request, String searchText) {
+
+
+		// get user session id,start time,end time,user id,user name, from Request
+		model.addAttribute("userSessionId", request.getParameter("userSessionId"));
+		model.addAttribute("startTime", request.getParameter("startTime"));
+		model.addAttribute("endTime", request.getParameter("endTime"));
+		model.addAttribute("userId", request.getParameter("userId"));
+		model.addAttribute("userName", request.getParameter("userName"));
+
+		pagination.setListName(USERSUMMARYSESSION);
+		try {
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("userActivityLogId");
+				userActivityServicesImpl.paginationPrev(null, pagination,
+						searchText, model);
+			} else {
+				userActivityServicesImpl.paginationPrev(null, pagination,
+						searchText, model);
+			}
+		} catch (Exception ex) {
+			LOGGER.error("exception while calling paginationPrev method.");
+		}
+		return USERSUMMARYSESSIONLISTVIEW;
+
+	}
+
+	/**
+	 * Post method for User List Pagination - Next
+	 * @param model
+	 * @param pagination
+	 * @param request
+	 * @param searchText
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/nextActionSummary" }, method = RequestMethod.POST)
+	public String nextPostActionSummary(Model model,
+			@ModelAttribute(PAGINATION) Pagination pagination,
+			String searchText, HttpServletRequest request) {
+
+		// get user session id,start time,end time,user id,user name, from Request
+		model.addAttribute("userSessionId", request.getParameter("userSessionId"));
+		model.addAttribute("startTime", request.getParameter("startTime"));
+		model.addAttribute("endTime", request.getParameter("endTime"));
+		model.addAttribute("userId", request.getParameter("userId"));
+		model.addAttribute("userName", request.getParameter("userName"));
+		
+		
+		pagination.setListName(USERSUMMARYSESSION);
+		try {
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("userActivityLogId");
+				userActivityServicesImpl.paginationNext(null, pagination,
+						searchText, model);
+			} else {
+				userActivityServicesImpl.paginationNext(null, pagination,
+						searchText, model);
+			}
+		} catch (Exception ex) {
+			LOGGER.error("exception while calling nextPost OESCLIENT User Controller  method.");
+		}
+		return USERSUMMARYSESSIONLISTVIEW;
+	}
+
+	
+
+	
+	/**
+	 * Post method for User List Pagination - Previous
+	 * @param model
+	 * @param pagination
+	 * @param request
+	 * @param searchText
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/prev" }, method = RequestMethod.POST)
+	public String prevPost(Model model,
+			@ModelAttribute(PAGINATION) Pagination pagination,
+			HttpServletRequest request, String searchText) {
+
+		// get OES client id from session
+		Client client = SessionHelper.getClient(request);
+		model.addAttribute("clientID", client.getClientID());
+
+		pagination.setListName(USERLIST);
+		try {
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("id");
+				oesClientUserServicesImplObj.paginationPrev(null, pagination,
+						searchText, model);
+			} else {
+				oesClientUserServicesImplObj.paginationPrev(null, pagination,
+						searchText, model);
+			}
+		} catch (Exception ex) {
+			LOGGER.error("exception while calling paginationPrev method.");
+		}
+		return USERLISTVIEW;
+
+	}
+	
+	/**
+	 * Post method for User List Pagination - Next
+	 * @param model
+	 * @param pagination
+	 * @param request
+	 * @param searchText
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/next" }, method = RequestMethod.POST)
+	public String nextPost(Model model,
+			@ModelAttribute(PAGINATION) Pagination pagination,
+			String searchText, HttpServletRequest request) {
+
+		// get OES client id from session
+		Client client = SessionHelper.getClient(request);
+		model.addAttribute("clientID", client.getClientID());
+
+		pagination.setListName(USERLIST);
+		try {
+			if (searchText != null && !(searchText.equals(""))) {
+				pagination.setPropertyName("id");
+				oesClientUserServicesImplObj.paginationNext(null, pagination,
+						searchText, model);
+			} else {
+				oesClientUserServicesImplObj.paginationNext(null, pagination,
+						searchText, model);
+			}
+		} catch (Exception ex) {
+			LOGGER.error("exception while calling nextPost OESCLIENT User Controller  method.");
+		}
+		return USERLISTVIEW;
+	}
+
+	
+
+
+
+	/**
+	 * Get method to Cancel Client User List
+	 * @param model
+	 * @return String this returns the path of a view
+	 */
+	@RequestMapping(value = { "/cancel" }, method = RequestMethod.GET)
+	public String cancelGet(Model model) {
+		try {
+			return "redirect:actionLog";
+		} catch (Exception ex) {
+			LOGGER.error("Excption while cancelGet in OESClientController:"+ex.getMessage());
+			model.addAttribute(EXCEPTIONSTRING, ex);
+			return ERRORPAGE;
+		}
+	}
+
+	
+	/**
+	 * Adds the message.
+	 *
+	 * @param request the request
+	 * @param model the model
+	 * @param locale the locale
+	 */
+	protected void addMessage(HttpServletRequest request, Model model,
+			Locale locale) {
+		String messageId = request.getParameter("messageid");
+		if (messageId != null && messageId.trim().length() != 0) {
+			MKCLUtility.addMessage(Integer.parseInt(messageId), model, locale);
+		}
+	}
+
+	
+	public void setApplicationContext(ApplicationContext applicationContext)throws BeansException {
+		oesClientUserServicesImplObj = (OESClientUserServicesImpl) applicationContext.getBean("OESClientUserService");
+		userServicesImpl = (UserSessionServicesImpl) applicationContext.getBean("UserSessionServices");
+		userActivityServicesImpl = (UserActivityLoggerServicesImpl) applicationContext.getBean("UserActivityLoggerServices");
+	}
+
+}
---
+++ Action_LOG_TASK_NAN/actionLog/userActivityLogList.jsp
@@ -0,0 +1,267 @@
+<%@page contentType="text/html;charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<html>
+<head>
+<title><spring:message code="actionLogSummaryList.title" /></title>
+<spring:message code="project.resources" var="resourcespath" />
+
+
+</head>
+
+<body>
+	
+	<spring:message code="global.dateFormatWithoutTime" var="dateFormat" />
+	<spring:message code="global.currencyCode" var="currencyCode" />
+	<fmt:setTimeZone value="${sessionScope.timeZone}" />
+	
+	
+	<div class="row">
+		<div class="col-sm main-header">
+			<h2>
+				<spring:message code="actionLogSummaryList.users" />
+			</h2>
+			<label><b><spring:message code="actionLogSummaryList.usernameKey" />  ${userName} </b></label>
+			<br>
+			<label><b><spring:message code="actionLogSummaryList.useridKey" /> ${userId} </b></label>
+			<br>
+			<label><b><spring:message code="actionLogSummaryList.login" /> </b> ${startTime}</label>
+			<label><b><spring:message code="actionLogSummaryList.logout" /> </b> ${endTime}</label>
+		</div>
+		<div class="col-sm">
+			<div  class="btn-group pull-right">
+				<a class="btn btn-main btn-rounded btn-long btn-sm" style="margin-bottom: 20px" href="userLogList"><spring:message code="actionLogSessionlist.userDetails" /></a> 
+			</div>
+		
+		</div>
+	</div>
+
+<div class="holder">
+
+		
+	<div ><!-- class="holder" -->
+		
+		
+
+		<c:if test="${UserSessionSummaryList != null}">
+			<!--Integrated data table  -->
+			<table class="table table-striped table-bordered" id="demotable" style="font-size: 15px;">
+				<thead>
+					<tr>
+						<th><spring:message code="actionLogSummaryList.startTime" /></th>
+						<th><spring:message code="actionLogSummaryList.endTime" /></th>
+						<th><spring:message code="actionLogSummaryList.actionPerformed" /></th>		
+						<th><spring:message code="actionLogSummaryList.duration" /></th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+
+					
+					<th><spring:message code="actionLogSummaryList.startTime" /></th>
+					<th><spring:message code="actionLogSummaryList.endTime" /></th>
+					<th><spring:message code="actionLogSummaryList.actionPerformed" /></th>	
+	                <th><spring:message code="actionLogSummaryList.duration" /></th>
+
+					</tr>
+				</tfoot>
+				<c:if test="${fn:length(UserSessionSummaryList) != 0}">
+					<tbody>
+						<c:forEach var="listObj" items="${UserSessionSummaryList}">
+							<tr>
+								<td>${listObj.startTime}</td>
+								<td>${listObj.endTime}</td>
+								<td>${listObj.userAction}</td>
+								<td>${listObj.duration}</td>	
+							</tr>
+						</c:forEach>
+					</tbody>
+				</c:if>
+			</table>
+			
+				<center>
+					<a href="../actionLog/userSessionLogList?userId=${userId}&userName=${userName}" class="btn btn-secondary btn-rounded btn-long btn-sm ">
+					<spring:message code="global.back" />
+					</a>
+			</center>
+			
+			<c:if test="${fn:length(UserSessionSummaryList) != 0}">
+				<!--Display count of records  -->
+				<div class="span5 ml-0">
+					<spring:message code="global.pagination.showing"></spring:message>
+					${pagination.start+1} - ${pagination.end}
+					<spring:message code="global.pagination.of"></spring:message>
+					<c:choose>
+						<c:when test="${disableNext==true}">
+							 ${pagination.end} 
+						</c:when>
+						<c:otherwise>
+							<spring:message code="actionLogSummaryList.ManyKey" />
+						</c:otherwise>
+					</c:choose>
+				</div>
+				
+				<!--Display pagination buttons  -->
+				<div class="dataTables_paginate paging_bootstrap pagination">
+						<ul>
+						<c:choose>
+							<c:when
+								test="${disablePrev==true || (fn:length(UserSessionSummaryList)<=pagination.recordsPerPage && pagination.start==0)}">
+								<li class="prev"><a href="#" id="prev_anchor_tag"
+									style="display: none;"><spring:message
+											code="global.previous"></spring:message>
+								</a></li>
+							</c:when>
+							<c:otherwise>
+								<li class="prev"><a href="#" id="prev_anchor_tag"><spring:message
+											code="global.previous"></spring:message>
+								</a></li>
+							</c:otherwise>
+						</c:choose>
+						<c:choose>
+							<c:when test="${disableNext==true}">
+								<li class="next"><a href="#" id="next_anchor_tag"
+									style="display: none;"><spring:message code="global.next"></spring:message>
+										→ </a></li>
+							</c:when>
+							<c:otherwise>
+								<li class="next"><a href="#" id="next_anchor_tag"><spring:message
+											code="global.next"></spring:message> → </a></li>
+							</c:otherwise>
+						</c:choose>
+					</ul>		
+					<!--Form For previous,next buttons  -->
+					<form:form action="prevActionSummary" method="POST" modelAttribute="pagination"
+						id="prev_selector_for_the_form" name="prev_selector_for_the_form">
+						<form:hidden path="end" />
+						<form:hidden path="start" />
+						<form:hidden path="recordsPerPage" />
+						<input type="hidden" id="searchText" name="searchText"
+							value="${searchText}" />
+						<input type="hidden" id="userId" name="userId"
+							value="${userId}" />
+						<input type="hidden" id="userName" name="userName"
+							value="${userName}" />
+						<input type="hidden" id="startTime" name="startTime"
+							value="${startTime}" />
+						<input type="hidden" id="endTime" name="endTime"
+							value="${endTime}" />
+						<input type="hidden" id="userSessionId" name="userSessionId"
+							value="${userSessionId}" />
+					</form:form>
+					<form:form action="nextActionSummary" method="POST" modelAttribute="pagination"
+						id="next_selector_for_the_form">
+						<form:hidden path="end" />
+						<form:hidden path="start" />
+						<form:hidden path="recordsPerPage" />
+						<input type="hidden" id="searchText" name="searchText"
+							value="${searchText}" />
+						<input type="hidden" id="userId" name="userId"
+							value="${userId}" />
+						<input type="hidden" id="userName" name="userName"
+							value="${userName}" />
+						<input type="hidden" id="startTime" name="startTime"
+							value="${startTime}" />
+						<input type="hidden" id="endTime" name="endTime"
+							value="${endTime}" />
+						<input type="hidden" id="userSessionId" name="userSessionId"
+							value="${userSessionId}" />
+					</form:form>
+				</div>
+			</c:if>
+		</c:if>
+</div>
+</div>
+	<script type="text/javascript"
+		src="<c:url value="${resourcespath}js/gridDataTable.js"></c:url>">
+		
+	</script>
+
+	<script type="text/javascript">
+		$(document)
+				.ready(
+						function() {
+
+							$('#demotable')
+									.dataTable(
+											{
+												"sDom" : "<'row-fluid'<'span4'><'span12'f>r>t<'row-fluid'<'span4'><'span8'>>",
+												"iDisplayLength" : -1,"bSort": false,
+												"aoColumns" : [ null, null,
+														null, {
+															"bSortable" : false
+														}, ],
+												"oTableTools" : {
+													"sSwfPath" : "../resources/media/csv_xls_pdf.swf",
+													"aButtons" : [
+															"copy",
+															"print",
+															{
+																"sExtends" : "collection",
+																"sButtonText" : 'Save <span class="caret" />',
+																"aButtons" : [
+																		{
+																			'sExtends' : 'csv',
+																			'mColumns' : [
+																					0,
+																					1,
+																					2,
+																					3,
+																					4 ]
+																		},
+																		{
+																			'sExtends' : 'xls',
+																			'mColumns' : [
+																					0,
+																					1,
+																					2,
+																					3,
+																					4 ]
+																		},
+																		{
+																			'sExtends' : 'pdf',
+																			'mColumns' : [
+																					0,
+																					1,
+																					2,
+																					3,
+																					4 ]
+																		} ]
+															} ]
+												}
+											});
+
+							$("#prev_anchor_tag").click(function() {
+								$("#prev_selector_for_the_form").submit();
+								return false;
+							});
+
+							$("#next_anchor_tag").click(function() {
+								$("#next_selector_for_the_form").submit();
+								return false;
+							});
+
+							$('#go')
+									.click(
+											function() {
+											
+												var searchText = $('#search').val();
+												var userSessionId = $('#userSessionId').val();
+												window.location.href = '../actionLog/userActivityLogList?searchText='+ searchText +'&userSessionId='+userSessionId;
+											});
+							$('#back')
+					        .click(
+					        		function(){
+					        			var userId = $('#userId').val();
+					        			var userName = $('#userName').val();
+					        			window.location.href='../actionLog/userSessionLogList?userId='+ userId +'&userName='+userName;
+					        		});
+
+
+						});
+	</script>
+</body>
+</html>
